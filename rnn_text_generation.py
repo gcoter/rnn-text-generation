ed995288fc5f5b225ee8b0ed327fb5d82dc121ce
gcoter <guillaume.coter@gmail.com>
2016-10-14 16:56:09 +0900

    Get and prepare data

---
+++ rnn_text_generation.py
@@ -0,0 +1,64 @@
+﻿"""
+Text Generation (character level) using RNN
+
+Inspired from http://machinelearningmastery.com/text-generation-lstm-recurrent-neural-networks-python-keras/
+
+I reimplemented the model with tensorflow
+"""
+
+from __future__ import print_function
+import codecs
+import numpy as np
+
+DATA_FOLDER = 'data/'
+DATA_PATH = DATA_FOLDER + 'wonderland.txt'
+
+# === GET THE DATA ===
+#with codecs.open(DATA_PATH, encoding='utf-8') as textfile:
+with open(DATA_PATH, 'r') as textfile:
+	raw_text = textfile.read()
+
+raw_text = raw_text.lower() # Convert to lower case to reduce the vocabulary used (characters)
+
+# === CREATE VOCABULARY ===
+chars = sorted(list(set(raw_text))) # This is the vocabulary
+char_to_int = dict((char, index) for index, char in enumerate(chars)) # Mapping from char to int
+int_to_char = dict((index, char) for index, char in enumerate(chars)) # Mapping from int to char
+
+raw_text_size = len(raw_text)
+vocabulary_size = len(chars)
+
+print(vocabulary_size,"characters in vocabulary")
+
+# === CREATE DATASETS ===
+SEQ_LENGTH = 100
+
+dataX = [] # Sequences of characters (converted to int)
+dataY = [] # Character to predict from sequences (converted to int)
+for i in range(0, raw_text_size - SEQ_LENGTH):
+	input_seq = raw_text[i:i + SEQ_LENGTH]
+	char_out = raw_text[i + SEQ_LENGTH]
+	dataX.append([char_to_int[char] for char in input_seq])
+	dataY.append(char_to_int[char_out])
+
+num_sequences = len(dataX)
+print("Total number of sequences in dataset: ", num_sequences)
+
+# === PREPARE DATASETS ===
+def to_categorical(data,vocabulary_size):
+	data_np = np.array(data)
+	res = np.zeros((len(data_np), vocabulary_size), dtype=np.bool)
+	res[np.arange(len(data_np)),data_np] = 1
+	return res
+
+# reshape X to be [samples, time steps]
+X = np.reshape(dataX, (num_sequences, SEQ_LENGTH))
+# normalize
+X = X / float(vocabulary_size)
+# one hot encode the output variable
+Y = to_categorical(dataY,vocabulary_size)
+
+print("X shape:",X.shape)
+print("Y shape:",Y.shape)
+
+# === DEFINE MODEL ===
